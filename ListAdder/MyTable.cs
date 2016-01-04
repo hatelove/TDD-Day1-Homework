db4d053a90ef4b4e658609784b068ff9ddf488cc
91 <hatelove@users.noreply.github.com>
2016-01-05 01:20:55 +0800

    Update MyTable.cs
    MyTable 的設計跟需求的意義搭不太起來。
    
    原因是需求上是11筆 row data, 從column的意義上來看可能是 products or orders。但這邊 MyTable 的設計是用 column 切分，變成四組 column。

--- ListAdder/MyTable.cs
+++ ListAdder/MyTable.cs
@@ -9,10 +9,15 @@ namespace ListAdder
     public class MyTable
     {
         private Dictionary<string, List<int>> table = new Dictionary<string, List<int>>();
+        //有可能傳入的data長度是不一樣的，這樣子是用4個column來當資料，而不是用11筆row來當資料
+        
         public void AddColumn(string header, List<int> data)
         {
             table[header] = data;
         }
+        
+        //取得資料應該是取得11筆資料，而不是取得某一欄的資料。例如延伸一個新的需求，要算出每個grouping的毛利平均，毛利是SellPrice/Cost。
+        //目前這樣的設計無法一次取得兩個 column 來運算。
         public List<int> GetColumnData(string header)
         {
             return table[header];
